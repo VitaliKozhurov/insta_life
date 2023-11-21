cb54a156dceaecbbc5996e224311a9105cae43d3
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-21 14:07:28 +0300

    refactor: refactor app folder structure

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import { App } from '@/application'
+
+import '@/application/index.scss'
+
+export default App
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { HeadMeta } from '@/shared'
+
+export default function Home() {
+  return (
+    <>
+      <HeadMeta title={'Main page'} />
+      <main>
+        <span>Main page </span>
+      </main>
+    </>
+  )
+}
