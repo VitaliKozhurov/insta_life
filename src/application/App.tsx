e4439f9a2eabab3b2d476203130fb907977b37dc
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-21 12:52:56 +0300

    chore: set folder structure, add styles, fonts, and etc

---
+++ src/application/App.tsx
@@ -0,0 +1,18 @@
+import { ReactElement, ReactNode } from 'react'
+
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
+
+type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
---
+++ src/shared/config/headMeta/index.ts
@@ -0,0 +1,1 @@
+export { HeadMeta } from './HeadMeta'
