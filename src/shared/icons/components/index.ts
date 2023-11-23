2f0fdca620d383316c654935eecb6f68785c6bd4
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-23 16:44:19 +0300

    feat: add styles and stories for checkbox

--- src/shared/icons/components/index.ts
+++ src/shared/icons/components/index.ts
@@ -1,3 +1,4 @@
+export { CheckBoxIcon } from './CheckBoxIcon'
 export { ClosedEyeIcon } from './ClosedEyeIcon'
 export { OpenEyeIcon } from './OpenedEyeIcon'
 export { SearchIcon } from './SearchIcon'
