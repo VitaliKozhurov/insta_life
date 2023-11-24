dfc9be761274516cdecf323e8d1cc80ba0950833
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-25 01:43:02 +0300

    feat: add stories for Select components

--- src/shared/ui/Select/Select.stories.tsx
+++ src/shared/ui/Select/Select.stories.tsx
@@ -2,7 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { useState } from 'react'
 
-import { Select } from './Select'
+import { inter } from '@/application'
+import { Flag } from '@/shared'
+import { action } from '@storybook/addon-actions'
+import russianFlag from 'public/flags/russianFlag.png'
+import ukFlag from 'public/flags/ukFlag.png'
+
+import { Option, Select, SelectProps } from './Select'
 
 const meta = {
   component: Select,
@@ -13,12 +19,77 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-const options = [
-  { title: 'Russian', value: 'russian' },
-  { title: 'English', value: 'english' },
-  { title: 'Spanish', value: 'spanish' },
+const options: Option[] = [
+  { className: inter.className, title: 'Russian', value: 'russian' },
+  { className: inter.className, title: 'English', value: 'english' },
+  { className: inter.className, title: 'Spanish', value: 'spanish' },
+]
+
+const optionsWithIcons: Option[] = [
+  {
+    className: inter.className,
+    icon: <Flag size={2} src={russianFlag} />,
+    title: 'Russian',
+    value: 'russian',
+  },
+  {
+    className: inter.className,
+    icon: <Flag size={2} src={ukFlag} />,
+    title: 'English',
+    value: 'english',
+  },
 ]
 
 export const Default: Story = {
-  args: { open: true, options: options, value: options[0].value },
+  args: {
+    label: 'Language select',
+    options: options,
+    value: options[0].value,
+  },
+}
+
+export const DefaultWithIcons: Story = {
+  args: {
+    label: 'Language select',
+    open: true,
+    options: optionsWithIcons,
+    value: optionsWithIcons[0].value,
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    label: 'Language select',
+    options: options,
+    value: options[0].value,
+  },
+}
+
+const SelectWithHooks = (args: SelectProps) => {
+  const [value, setValue] = useState(args.options[0].value)
+  const onValueChange = (value: string) => {
+    setValue(value)
+    action('Set language: ')(value)
+  }
+
+  return (
+    <>
+      <Select onValueChange={onValueChange} options={args.options} value={value} />
+    </>
+  )
+}
+
+export const Controlled: Story = {
+  args: {
+    options: options,
+  },
+  render: args => <SelectWithHooks {...args} />,
+}
+
+export const ControlledWithIcons: Story = {
+  args: {
+    options: optionsWithIcons,
+  },
+  render: args => <SelectWithHooks {...args} />,
 }
