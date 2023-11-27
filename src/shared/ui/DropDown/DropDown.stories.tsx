13aea93591d9bd4357990e9c07c7dfb531afd610
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-27 15:57:04 +0300

    refactor: refactor dropDown

--- src/shared/ui/DropDown/DropDown.stories.tsx
+++ src/shared/ui/DropDown/DropDown.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { CSSProperties } from 'react'
+
+import { inter } from '@/application'
 import { BellIcon } from '@/shared'
 
 import { DropDown } from './DropDown'
@@ -13,9 +16,37 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+const titleStyle: CSSProperties = {
+  borderBottom: '1px solid var(--color-dark-100)',
+  fontSize: '1.6rem',
+  fontWeight: 500,
+  margin: 0,
+  paddingBottom: '1.2rem',
+}
+
+const Title = () => {
+  return <h3 style={titleStyle}>{'Уведомления'}</h3>
+}
+
 export const Default: Story = {
   args: {
-    children: <h1>Example</h1>,
-    trigger: <BellIcon />,
+    children: (
+      <>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+        <h1>Example text</h1>
+      </>
+    ),
+    className: inter.className,
+    dropDownTitle: <Title />,
+    trigger: <BellIcon size={2} />,
   },
 }
--- src/shared/ui/DropDown/DropDown.tsx
+++ src/shared/ui/DropDown/DropDown.tsx
@@ -1,23 +1,38 @@
-import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef, useState } from 'react'
 
 import * as RadixDropDown from '@radix-ui/react-dropdown-menu'
+import clsx from 'clsx'
 
+import s from './DropDown.module.scss'
 type DropDownProps = {
   children: ReactNode
+  dropDownTitle?: ReactNode
   trigger: ReactNode
 } & ComponentPropsWithoutRef<typeof RadixDropDown.Content>
 
 export const DropDown = forwardRef<ElementRef<typeof RadixDropDown.Content>, DropDownProps>(
-  ({ children, className, trigger, ...restProps }, ref) => {
-    return (
-      <RadixDropDown.Root>
-        <RadixDropDown.Trigger asChild>
-          <button>{trigger}</button>
-        </RadixDropDown.Trigger>
+  ({ children, className, dropDownTitle, trigger, ...restProps }, ref) => {
+    const [open, setOpen] = useState(false)
+    const classNames = {
+      arrow: s.arrow,
+      content: clsx(s.content, className),
+      dropDownTitle: s.dropDownTitle,
+      scrollContainer: s.scrollContainer,
+      trigger: s.trigger,
+    }
 
+    return (
+      <RadixDropDown.Root onOpenChange={setOpen} open={open}>
+        <RadixDropDown.Trigger asChild>
+          <button className={classNames.trigger}>{trigger}</button>
+        </RadixDropDown.Trigger>
         <RadixDropDown.Portal>
-          <RadixDropDown.Content className={'DropdownMenuContent'} ref={ref} {...restProps}>
-            {children}
+          <RadixDropDown.Content className={classNames.content} ref={ref} {...restProps}>
+            <RadixDropDown.Arrow asChild className={classNames.arrow}>
+              <div></div>
+            </RadixDropDown.Arrow>
+            {dropDownTitle && dropDownTitle}
+            <div className={classNames.scrollContainer}>{children}</div>
           </RadixDropDown.Content>
         </RadixDropDown.Portal>
       </RadixDropDown.Root>
