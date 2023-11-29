93dc0cedca3366761049125baeacc57b4b7bfb11
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-29 16:40:09 +0300

    refactor: refactor SignUp form

--- src/features/registration/registrationByForm/ui/SignUpForm.tsx
+++ src/features/registration/registrationByForm/ui/SignUpForm.tsx
@@ -1,14 +1,25 @@
 import { useForm } from 'react-hook-form'
 
 import { ControlledCheckbox, ControlledInput } from '@/shared/controlledUI'
+import { Typography } from '@/shared/ui'
 
 export const SignUpForm = () => {
   const { control } = useForm()
 
   return (
     <form>
-      <ControlledInput control={control} name={'123'} />
-      <ControlledCheckbox control={control} name={'checkbox'} />
+      <ControlledInput control={control} label={'Username'} name={'username'} />
+      <ControlledInput control={control} label={'Email'} name={'email'} />
+      <ControlledInput control={control} label={'Password'} name={'password'} type={'password'} />
+      <ControlledInput
+        control={control}
+        label={'Password confirmation'}
+        name={'passwordConfirmation'}
+        type={'password'}
+      />
+
+      <ControlledCheckbox control={control} name={'agree'} />
+      <Typography></Typography>
     </form>
   )
 }
