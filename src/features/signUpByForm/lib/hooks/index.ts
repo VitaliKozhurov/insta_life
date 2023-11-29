1b7e7c907a9bcdfabd18b25c2bab35a10c6856ee
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-30 00:20:32 +0300

    refactor: refactor signUp form

--- src/features/signUpByForm/lib/hooks/index.ts
+++ src/features/signUpByForm/lib/hooks/index.ts
@@ -1,1 +1,2 @@
 export { useSignUpForm } from './useSignUpForm'
+export type { SignUpFormValuesType } from './useSignUpForm'
---
+++ src/features/signUpByForm/lib/hooks/useSignUpForm.ts
@@ -0,0 +1,37 @@
+import { useForm } from 'react-hook-form'
+
+import { PASSWORD_PATTERN, USERNAME_PATTERN } from '@/shared/constants'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const signUpSchema = z
+  .object({
+    email: z.string().email({ message: 'email' }).trim(),
+    password: z.string().min(3, '3').max(20, '20').regex(PASSWORD_PATTERN, 'regex').trim(),
+    passwordConfirmation: z
+      .string()
+      .min(3, '3')
+      .max(20, '20')
+      .regex(PASSWORD_PATTERN, 'regex')
+      .trim(),
+    policyAgreement: z.boolean().optional(),
+    username: z.string().min(3, '3').max(30, '30').regex(USERNAME_PATTERN, 'regex').trim(),
+  })
+  .refine(data => data.password === data.passwordConfirmation, {
+    message: 'Those passwords didn’t match. Try again.',
+    path: ['passwordConfirmation'],
+  })
+
+export type SignUpFormValuesType = z.infer<typeof signUpSchema>
+
+export const useSignUpForm = () =>
+  useForm<SignUpFormValuesType>({
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      policyAgreement: false,
+      username: '',
+    },
+    resolver: zodResolver(signUpSchema),
+  })
