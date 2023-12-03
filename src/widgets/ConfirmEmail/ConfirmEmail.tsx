a88c18ee5806ce456bac3d95cfd50db7f3c3c0b0
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-03 22:30:24 +0300

    feat: add email confirmation widget, move image from shared to public folder

---
+++ src/widgets/ConfirmEmail/ConfirmEmail.tsx
@@ -0,0 +1,35 @@
+import { Routes, TypographyVariant } from '@/shared/types'
+import { Button, Typography } from '@/shared/ui'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import s from './ConfirmEmail.module.scss'
+
+import successEmailConfirmation from '/public/confirm_email.png'
+
+export const ConfirmEmail = () => {
+  const classNames = {
+    imageWrapper: s.imageWrapper,
+    link: s.link,
+    root: s.root,
+    text: s.text,
+    title: s.title,
+  }
+
+  return (
+    <section className={classNames.root}>
+      <Typography as={'h1'} className={classNames.title} variant={TypographyVariant.H1}>
+        Congratulations!
+      </Typography>
+      <Typography className={classNames.text} variant={TypographyVariant.Regular_16}>
+        Your email has been confirmed
+      </Typography>
+      <Button as={Link} className={classNames.link} href={Routes.SIGN_IN}>
+        Sign In
+      </Button>
+      <div className={classNames.imageWrapper}>
+        <Image alt={'Email confirmed'} src={successEmailConfirmation} />
+      </div>
+    </section>
+  )
+}
