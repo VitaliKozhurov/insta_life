231de13060259c54344be0537a454cd37d21b4db
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-07 12:27:04 +0300

    feat: create app store

---
+++ src/application/store.ts
@@ -0,0 +1,9 @@
+import { baseApi } from '@/shared'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+})
