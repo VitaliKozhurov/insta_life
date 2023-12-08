148da2d363e05775fdd54801719d7b71ca95d95d
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-08 12:55:44 +0300

    feat: add utils for process errors on request

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { BASE_API_URL } from '../constants'
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL, credentials: 'include' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL /*credentials: 'include'*/ }),
   endpoints: () => ({}),
   reducerPath: 'baseApi',
 })
---
+++ src/shared/lib/errorHandlers.ts
@@ -0,0 +1,62 @@
+import { FieldError } from 'react-hook-form'
+
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
+
+const isFetchBaseQueryError = (error: unknown): error is FetchBaseQueryError => {
+  return typeof error === 'object' && error != null && 'status' in error
+}
+
+const isUserDataValidationError = (
+  data: unknown
+): data is {
+  message: {
+    field: string
+    message: string
+  }[]
+} => {
+  return (
+    typeof data === 'object' &&
+    data != null &&
+    'message' in data &&
+    Array.isArray(data.message) &&
+    data.message.every(item => 'field' in item && 'message' in item)
+  )
+}
+
+const isErrorWithMessageInData = (data: unknown): data is { message: string } => {
+  return (
+    typeof data === 'object' &&
+    data != null &&
+    'message' in data &&
+    typeof data.message === 'string'
+  )
+}
+
+const isFetchError = (error: unknown): error is { error: string; status: 'FETCH_ERROR' } => {
+  return (
+    typeof error === 'object' &&
+    error != null &&
+    'status' in error &&
+    error.status === 'FETCH_ERROR' &&
+    'error' in error &&
+    typeof error.error === 'string'
+  )
+}
+
+export const onRequestErrorHandler = <T>(
+  error: unknown,
+  setError: (name: T, error: FieldError) => void
+) => {
+  if (isFetchBaseQueryError(error) && error.data) {
+    if (isUserDataValidationError(error.data)) {
+      error.data.message.forEach(item =>
+        setError(item.field as T, { message: item.message, type: 'validationError' })
+      )
+    } else if (isErrorWithMessageInData(error.data)) {
+      alert(error.data.message)
+    }
+  }
+  if (isFetchError(error)) {
+    alert(error.error)
+  }
+}
