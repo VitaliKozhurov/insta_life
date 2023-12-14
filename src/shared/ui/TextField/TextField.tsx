5acc0971e579373c7258304d1f502f12f50cdd9d
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-14 13:07:45 +0300

    refactor: refactor TextField component

--- src/shared/ui/TextField/TextField.tsx
+++ src/shared/ui/TextField/TextField.tsx
@@ -1,22 +1,32 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ChangeEvent, ComponentPropsWithoutRef } from 'react'
+
+import clsx from 'clsx'
 
 import s from './TextField.module.scss'
-type Props = { error: string; label: string } & ComponentPropsWithoutRef<'textarea'>
+export type TextFieldProps = { error: string; label: string } & ComponentPropsWithoutRef<'textarea'>
 
-export const TextField = ({ children, className, disabled, error, label, ...restProps }: Props) => {
+export const TextField = ({ className, error, label, onChange, ...restProps }: TextFieldProps) => {
   const classNames = {
     error: s.error,
-    label: s.label,
+    label: clsx(s.label, restProps.disabled && s.disabledLabel),
     root: s.root,
-    text: s.text,
+    text: clsx(s.text, !!restProps.value && s.active, error && s.withError),
+  }
+
+  const onChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    onChange && onChange(event)
+    event.target.style.height = 'auto'
+    event.target.style.height = `${event.target.scrollHeight}px`
   }
 
   return (
     <div className={classNames.root}>
-      {label && <label className={classNames.label}>{label}</label>}
-      <textarea className={classNames.text} {...restProps}>
-        {children}
-      </textarea>
+      {label && (
+        <label className={classNames.label} htmlFor={label}>
+          {label}
+        </label>
+      )}
+      <textarea className={classNames.text} id={label} onChange={onChangeHandler} {...restProps} />
       {error && <span className={classNames.error}>{error}</span>}
     </div>
   )
