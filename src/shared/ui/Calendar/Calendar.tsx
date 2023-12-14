9a46bef2189b7959150e2bbb370e3e7c8c849195
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-15 01:08:44 +0300

    feat: refactor today day style

--- src/shared/ui/Calendar/Calendar.tsx
+++ src/shared/ui/Calendar/Calendar.tsx
@@ -1,5 +1,7 @@
-import { ComponentPropsWithoutRef, useState } from 'react'
+import { useState } from 'react'
 import { DayPicker } from 'react-day-picker'
+
+import s from './Calendar.module.scss'
 
 type Props = {
   mode: 'multiple' | 'range' | 'single'
@@ -8,5 +10,24 @@ type Props = {
 export const Calendar = ({ mode }: Props) => {
   const [selected, setSelected] = useState<Date>()
 
-  return <DayPicker mode={mode} showOutsideDays />
+  return (
+    <DayPicker
+      classNames={{
+        cell: s.cell,
+        day: s.day,
+        day_outside: s.day_outside,
+        day_today: s.day_today,
+        head: s.head,
+        head_cell: s.head_cell,
+        head_row: s.head_row,
+        root: s.root,
+        row: s.row,
+        table: s.table,
+        tbody: s.tbody,
+      }}
+      mode={'single'}
+      showOutsideDays
+      weekStartsOn={1}
+    />
+  )
 }
