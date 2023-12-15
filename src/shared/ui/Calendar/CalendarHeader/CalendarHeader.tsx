d407e8f4807eb9d0710f119867e2e18e10d4e02f
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-15 22:11:09 +0300

    refactor: refactor Calendar Header

--- src/shared/ui/Calendar/CalendarHeader/CalendarHeader.tsx
+++ src/shared/ui/Calendar/CalendarHeader/CalendarHeader.tsx
@@ -1,10 +1,15 @@
 import { CaptionProps, useNavigation } from 'react-day-picker'
 
+import { ArrowLeftIcon, ArrowRightIcon, firstLetterUppercaseTransform } from '@/shared'
 import { format } from 'date-fns'
+import { enUS, ru } from 'date-fns/locale'
+import { useRouter } from 'next/router'
 
 import s from './CalendarHeader.module.scss'
 
 export const CalendarHeader = ({ displayMonth }: CaptionProps) => {
+  const { locale } = useRouter()
+  const currentLocale = locale === 'ru' ? ru : enUS
   const { goToMonth, nextMonth, previousMonth } = useNavigation()
 
   const goPreviousMonthHandler = () => {
@@ -13,16 +18,19 @@ export const CalendarHeader = ({ displayMonth }: CaptionProps) => {
   const goNextMonthHandler = () => {
     nextMonth && goToMonth(nextMonth)
   }
+  const month = firstLetterUppercaseTransform(
+    format(displayMonth, 'LLLL yyyy', { locale: currentLocale })
+  )
 
   return (
     <div className={s.root}>
-      <h3 className={s.title}>{format(displayMonth, 'MMMM yyyy')}</h3>
+      <h3 className={s.title}>{month}</h3>
       <div className={s.controls}>
         <button className={s.control} disabled={!previousMonth} onClick={goPreviousMonthHandler}>
-          Previous
+          <ArrowLeftIcon size={2} />
         </button>
         <button className={s.control} disabled={!nextMonth} onClick={goNextMonthHandler}>
-          Next
+          <ArrowRightIcon size={2} />
         </button>
       </div>
     </div>
