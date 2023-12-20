85186c90e985eebc0f0821abcaa490cc2d62a4d5
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-20 12:43:07 +0300

    feat: add url for auth with google, remove unused hook

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,1 +1,8 @@
-export const BASE_API_URL = 'https://ft-inctagram.site/api/v1'
+const BASE_URL = 'https://ft-inctagram.site/'
+const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID
+
+export const BASE_API_URL = `${BASE_URL}api/v1`
+export const FRONTEND_REDIRECT_URL =
+  (process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : BASE_URL) + 'auth/google'
+
+export const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=email profile&response_type=code&redirect_uri=${FRONTEND_REDIRECT_URL}&client_id=${CLIENT_ID}`
