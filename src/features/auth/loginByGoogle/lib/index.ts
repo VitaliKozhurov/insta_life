3d90a5218bd9ad887d1db295b4d8aefe1b419b18
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-20 12:47:07 +0300

    feat: add login by google

--- src/features/auth/loginByGoogle/lib/index.ts
+++
@@ -1,1 +0,0 @@
-export { useLoginByGoogle } from './useLoginByGoogle'
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,2 +1,2 @@
-export { BASE_API_URL } from './api'
+export { BASE_API_URL, GOOGLE_URL } from './api'
 export { PASSWORD_PATTERN, USERNAME_PATTERN } from './regex'
