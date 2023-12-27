3c08a6b60f61033d68269bf2c7b44cd916ac661b
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-27 10:07:38 +0300

    refactor: refactor ProfileInfoForm

--- src/widgets/ProfileInfoForm/ProfileInfoForm.tsx
+++ src/widgets/ProfileInfoForm/ProfileInfoForm.tsx
@@ -1,18 +1,51 @@
 import { useForm } from 'react-hook-form'
 
-import { ControlledInput, TextField } from '@/shared'
-import { DatePicker } from '@/widgets/DatePicker/DatePicker'
+import { ControlledInput, ControlledSelect, SelectOptions, TextField } from '@/shared'
+import { DatePicker } from '@/widgets'
 
 import s from './ProfileInfoForm.module.scss'
+
+const countriesOptions: SelectOptions[] = [
+  { title: 'Belarus', value: 'belarus' },
+  { title: 'Russia', value: 'russia' },
+]
+
+const citiesOptions: SelectOptions[] = [
+  { title: 'Minsk', value: 'minsk' },
+  { title: 'Moscow', value: 'moscow' },
+]
+
 export const ProfileInfoForm = () => {
   const { control } = useForm()
 
   return (
     <form className={s.form}>
-      <ControlledInput control={control} name={'userName'} />
-      <ControlledInput control={control} name={'firstName'} />
-      <ControlledInput control={control} name={'lastName'} />
+      <ControlledInput control={control} isRequired label={'Username'} name={'userName'} />
+      <ControlledInput control={control} isRequired label={'First Name'} name={'firstName'} />
+      <ControlledInput control={control} isRequired label={'Last Name'} name={'lastName'} />
       <DatePicker mode={'single'} />
+      <div className={s.selectWrapper}>
+        <div className={s.select}>
+          <ControlledSelect
+            className={s.select}
+            control={control}
+            fullWidth
+            label={'Select your country'}
+            name={'country'}
+            options={countriesOptions}
+          />
+        </div>
+        <div className={s.select}>
+          <ControlledSelect
+            className={s.select}
+            control={control}
+            fullWidth
+            label={'Select your city'}
+            name={'city'}
+            options={citiesOptions}
+          />
+        </div>
+      </div>
       <TextField fullWidth label={'About me'} />
     </form>
   )
