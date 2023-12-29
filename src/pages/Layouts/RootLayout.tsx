edbfd5147da6d3bef9ad500c0a9c4dc5d164148b
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-30 02:48:16 +0300

    refactor: refactor RootLayout for private route

--- src/pages/Layouts/RootLayout.tsx
+++ src/pages/Layouts/RootLayout.tsx
@@ -1,36 +1,40 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode } from 'react'
 
 import { inter } from '@/application'
-import { Loader, Routes, getFromLocalStorage } from '@/shared'
+import { useAuthMeQuery } from '@/pages/Layouts/authApi'
+import { Loader, Routes } from '@/shared'
 import { Header } from '@/widgets'
 import { useRouter } from 'next/router'
 
 type Props = { children: ReactNode }
 
 export const RootLayout = ({ children }: Props) => {
-  const [loading, setLoading] = useState(true)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const router = useRouter()
+  // test for private toute
+  const { data, isError, isLoading } = useAuthMeQuery()
 
-  /*  console.log(isLoggedIn)
-  useEffect(() => {
-    const token = getFromLocalStorage('token')
-
-    if (!token) {
-      router.push(Routes.SIGN_IN)
-    } else {
-      setIsLoggedIn(true)
-    }
-    setLoading(false)
-  }, [])
-
-  if (loading) {
+  if (isLoading) {
     return <Loader />
-  }*/
+  }
+
+  const isAuth = !isError
+
+  console.log(isAuth)
+  if (!isAuth && router.pathname !== '/sign-in') {
+    router.push(Routes.SIGN_IN)
+
+    return <Loader />
+  }
+
+  if (isAuth && router.pathname === '/sign-in') {
+    router.push(Routes.HOME)
+
+    return <Loader />
+  }
 
   return (
     <div className={inter.className}>
-      <Header isLoggedIn={isLoggedIn} />
+      <Header isLoggedIn={isAuth} />
       <>{children}</>
     </div>
   )
