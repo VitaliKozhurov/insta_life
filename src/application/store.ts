a5adfb9f4ff929c56c577f15ea4a610066411522
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-30 02:46:36 +0300

    feat: add middleware with authApi to store

--- src/application/store.ts
+++ src/application/store.ts
@@ -1,11 +1,19 @@
+import { useDispatch } from 'react-redux'
+
+import { authApi } from '@/pages/Layouts/authApi'
 import { baseApi } from '@/shared'
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(baseApi.middleware).concat(authApi.middleware),
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
 })
 setupListeners(store.dispatch)
+// test
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
