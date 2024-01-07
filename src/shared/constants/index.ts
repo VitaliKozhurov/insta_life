2a8d6be3e930dba3e145e3c252b3fb2a0fd33c6c
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-07 20:28:15 +0300

    feat: add array of Private Routes

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,2 +1,3 @@
 export { BASE_API_URL, GOOGLE_URL } from './api'
+export { PRIVATE_ROUTES } from './privateRoutes'
 export { PASSWORD_PATTERN, USERNAME_PATTERN } from './regex'
---
+++ src/shared/constants/privateRoutes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@/shared'
+
+export const PRIVATE_ROUTES = [
+  Routes.HOME,
+  Routes.MESSENGER,
+  Routes.CREATE,
+  Routes.SEARCH,
+  Routes.PROFILE,
+  Routes.SEARCH,
+  Routes.STATISTICS,
+  Routes.FAVORITES,
+]
