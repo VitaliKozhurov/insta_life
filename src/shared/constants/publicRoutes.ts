243f592f1f6c680ca0979afc32211ba426b9cd6b
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-07 21:41:58 +0300

    feat: add Public Routes

---
+++ src/shared/constants/publicRoutes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@/shared'
+
+export const PUBLIC_ROUTES = [
+  Routes.SIGN_IN,
+  Routes.SIGN_UP,
+  Routes.FORGOT_PASSWORD,
+  Routes.PRIVACY_POLICY,
+  Routes.TERMS_OF_SERVICE,
+]
