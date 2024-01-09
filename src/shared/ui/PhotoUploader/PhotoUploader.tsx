1d8d68b0c3617a5559037fa31c795b0046526af3
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-09 13:40:24 +0300

    feat: add PhotoUploader

--- src/shared/ui/PhotoUploader/PhotoUploader.tsx
+++ src/shared/ui/PhotoUploader/PhotoUploader.tsx
@@ -1,6 +1,9 @@
-import { ChangeEvent, ReactNode, useRef } from 'react'
+import { ReactNode, useRef } from 'react'
+import { Controller } from 'react-hook-form'
 
 import { Button, ButtonVariant } from '@/shared'
+
+import { useUploadFile } from './lib'
 
 type Props = {
   children: ReactNode
@@ -9,30 +12,50 @@ type Props = {
 }
 
 export const PhotoUploader = ({ children, className, onSelectPhoto }: Props) => {
+  const {
+    control,
+    formState: { defaultValues, errors },
+    getValues,
+  } = useUploadFile()
+
+  // console.log(getValues('image'))
+  // console.log(errors.image?.message)
+  // console.log(defaultValues)
   const fileInputRef = useRef<HTMLInputElement | null>(null)
   const onButtonClickHandler = () => fileInputRef.current?.click()
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files?.length) {
-      return
-    }
-    const fileUploaded = e.target.files[0]
 
-    onSelectPhoto(fileUploaded)
-  }
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   if (!e.target.files?.length) {
+  //     return
+  //   }
+  //   const fileUploaded = e.target.files[0]
+  //
+  //   onSelectPhoto(fileUploaded)
+  // }
 
   return (
-    <>
-      <Button className={className} onClick={onButtonClickHandler} variant={ButtonVariant.PRIMARY}>
+    <form>
+      <Button
+        className={className}
+        onClick={onButtonClickHandler}
+        type={'button'}
+        variant={ButtonVariant.PRIMARY}
+      >
         {children}
       </Button>
-      <input
-        accept={'image/*'}
-        multiple={false}
-        onChange={handleChange}
-        ref={fileInputRef}
-        style={{ display: 'none' }}
-        type={'file'}
+      <Controller
+        control={control}
+        name={'image'}
+        render={({ field: { name, onChange, value } }) => (
+          <input
+            name={name}
+            onChange={e => onChange(e.target.files?.[0])}
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            type={'file'}
+          />
+        )}
       />
-    </>
+    </form>
   )
 }
