af500a7352cdbe7468f734af13ab9310b7db1d3a
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-09 13:41:38 +0300

    feat: add useUpload hook

---
+++ src/shared/ui/PhotoUploader/lib/useUploadFile.ts
@@ -0,0 +1,30 @@
+import { useForm } from 'react-hook-form'
+
+const MAX_FILE_SIZE = 10000000
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/png']
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const uploadFileSchema = () =>
+  z.object({
+    image: z.custom<File>().refine(file => {
+      console.log(typeof file)
+      // file?.size <= MAX_FILE_SIZE
+    }, `The photo must be less than 10 Mb `),
+    // .refine(
+    //   file => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+    //   'The photo must have JPEG or PNG format'
+    // ),
+  })
+
+export type UploadFileValuesType = z.infer<ReturnType<typeof uploadFileSchema>>
+
+export const useUploadFile = () =>
+  useForm<UploadFileValuesType>({
+    defaultValues: {
+      image: undefined,
+    },
+    mode: 'onChange',
+    resolver: zodResolver(uploadFileSchema()),
+  })
