6a8906384b9794fb4c6cab408b5514d9e5aa3906
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-11 01:14:56 +0300

    feat: add utils function for crop image

--- src/features/profile/addPhotoModal/ui/AddingPhotoModal.tsx
+++ src/features/profile/addPhotoModal/ui/AddingPhotoModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import Cropper, { Area, Point } from 'react-easy-crop'
 
 import { ImageIcon, Modal, Nullable, PhotoUploader } from '@/shared'
@@ -15,8 +15,9 @@ type Props = {
 
 export const AddingPhotoModal = ({ onOpenChange, open }: Props) => {
   const [image, setImage] = useState<Nullable<File>>(null)
+  const [croppedImage, setCroppedImage] = useState<Nullable<Blob>>(null)
   const [error, setError] = useState<Nullable<string>>(null)
-  const imagePreview = image && !error ? URL.createObjectURL(image) : null
+  const imagePreview = useMemo(() => (image && !error ? URL.createObjectURL(image) : null), [image])
   const classNames = {
     button: s.button,
     content: s.content,
@@ -41,6 +42,9 @@ export const AddingPhotoModal = ({ onOpenChange, open }: Props) => {
     }
     onOpenChange(value)
   }
+  const onCropImage = (imageFile: Blob) => {
+    setCroppedImage(imageFile)
+  }
 
   return (
     <Modal
@@ -52,10 +56,15 @@ export const AddingPhotoModal = ({ onOpenChange, open }: Props) => {
       <div className={classNames.root}>
         {error && <p className={classNames.error}>{error}</p>}
         <div className={classNames.photo}>
-          {imagePreview && <Crop imagePreview={imagePreview} />}
+          {imagePreview && <Crop imagePreview={imagePreview} onCropImage={onCropImage} />}
           {!imagePreview && <ImageIcon size={4.8} />}
         </div>
-        <PhotoUploader className={classNames.button} onError={setError} onSelectPhoto={setImage}>
+        <PhotoUploader
+          className={classNames.button}
+          croppedImage={croppedImage}
+          onError={setError}
+          onSelectPhoto={setImage}
+        >
           Select from Computer
         </PhotoUploader>
       </div>
@@ -63,54 +72,112 @@ export const AddingPhotoModal = ({ onOpenChange, open }: Props) => {
   )
 }
 
-const Crop = ({ imagePreview }: any) => {
+type CropType = {
+  imagePreview: string
+  onCropImage: (imageFile: Blob) => void
+}
+
+const Crop = ({ imagePreview, onCropImage }: CropType) => {
   const [crop, setCrop] = useState<Point>({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
-  const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
-    console.log(croppedArea, croppedAreaPixels)
+
+  const onCropComplete = async (croppedArea: Area, croppedAreaPixels: Area) => {
+    try {
+      const imageFile = await getCroppedImg(imagePreview ?? '', croppedAreaPixels)
+
+      imageFile && onCropImage(imageFile)
+    } catch (e) {
+      // Todo add error toast
+      console.log(e)
+    }
   }
 
   return (
-    <Cropper
-      aspect={1}
-      crop={crop}
-      cropShape={'round'}
-      image={imagePreview}
-      objectFit={'cover'}
-      onCropChange={setCrop}
-      onCropComplete={onCropComplete}
-      onZoomChange={setZoom}
-      showGrid={false}
-      style={{ containerStyle: { border: '1px solid black', height: '100%' } }}
-      zoom={zoom}
-    />
+    <>
+      <Cropper
+        aspect={1}
+        crop={crop}
+        cropShape={'round'}
+        image={imagePreview}
+        objectFit={'cover'}
+        onCropChange={setCrop}
+        onCropComplete={onCropComplete}
+        onZoomChange={setZoom}
+        showGrid={false}
+        zoom={zoom}
+      />
+    </>
   )
 }
 
-// const Crop2 = ({ imagePreview }: any) => {
-//   const [crop, setCrop] = useState<Crop>({
-//     height: 50,
-//     unit: '%',
-//     width: 50,
-//     x: 25,
-//     y: 25,
-//   })
-//   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
-//
-//   console.log(imagePreview)
-//
-//   return (
-//     <ReactCrop
-//       aspect={1}
-//       circularCrop
-//       crop={crop}
-//       keepSelection
-//       minHeight={48}
-//       minWidth={48}
-//       onChange={c => setCrop(c)}
-//       style={{ minHeight: '100%' }}
-//     >
-//       <img alt={'asd'} src={imagePreview} />
-//     </ReactCrop>
-//   )
-// }
+export const createImage = (url: string): Promise<HTMLImageElement> =>
+  new Promise((resolve, reject) => {
+    const image = new Image()
+
+    image.addEventListener('load', () => resolve(image))
+    image.addEventListener('error', error => reject(error))
+    image.setAttribute('crossOrigin', 'anonymous')
+    image.src = url
+  })
+
+export function getRadianAngle(degreeValue: number) {
+  return (degreeValue * Math.PI) / 180
+}
+
+export function rotateSize(width: number, height: number, rotation: number) {
+  const rotRad = getRadianAngle(rotation)
+
+  return {
+    height: Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
+    width: Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
+  }
+}
+
+export default async function getCroppedImg(
+  imageSrc: string,
+  pixelCrop: Area,
+  rotation = 0,
+  flip = { horizontal: false, vertical: false }
+): Promise<Blob | null> {
+  const image = await createImage(imageSrc)
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+
+  if (!ctx) {
+    return null
+  }
+
+  const rotRad = getRadianAngle(rotation)
+
+  const { height: bBoxHeight, width: bBoxWidth } = rotateSize(image.width, image.height, rotation)
+
+  canvas.width = bBoxWidth
+  canvas.height = bBoxHeight
+
+  ctx.translate(bBoxWidth / 2, bBoxHeight / 2)
+  ctx.rotate(rotRad)
+  ctx.scale(flip.horizontal ? -1 : 1, flip.vertical ? -1 : 1)
+  ctx.translate(-image.width / 2, -image.height / 2)
+
+  ctx.drawImage(image, 0, 0)
+
+  const data = ctx.getImageData(pixelCrop.x, pixelCrop.y, pixelCrop.width, pixelCrop.height)
+
+  canvas.width = pixelCrop.width
+  canvas.height = pixelCrop.height
+
+  ctx.putImageData(data, 0, 0)
+
+  return new Promise((resolve, reject) => {
+    try {
+      canvas.toBlob(file => {
+        if (file) {
+          resolve(file)
+        }
+        resolve(null)
+      }, 'image/jpeg')
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
