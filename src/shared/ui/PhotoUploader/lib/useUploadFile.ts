248b74a6725e233ee2cb8e8a82dc456b175e86cb
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-12 00:29:46 +0300

    refactor: refactor useUpload hook

--- src/shared/ui/PhotoUploader/lib/useUploadFile.ts
+++ src/shared/ui/PhotoUploader/lib/useUploadFile.ts
@@ -3,27 +3,27 @@ import { useForm } from 'react-hook-form'
 const MAX_FILE_SIZE = 10000000
 const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/png']
 
+import { LocalesType } from '@/shared'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
-const uploadFileSchema = () =>
+type Props = LocalesType['profilePage']['management']['photoUploader']['modal']['errors']
+
+const uploadFileSchema = (t: Props) =>
   z.object({
     image: z
       .custom<File>()
-      .refine(file => file?.size <= MAX_FILE_SIZE, `The photo size must be less than 10 Mb`)
-      .refine(
-        file => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-        'The photo must have JPEG or PNG format'
-      ),
+      .refine(file => file?.size <= MAX_FILE_SIZE, t.maxSize)
+      .refine(file => ACCEPTED_IMAGE_TYPES.includes(file?.type), t.imageType),
   })
 
 export type UploadFileValuesType = z.infer<ReturnType<typeof uploadFileSchema>>
 
-export const useUploadFile = () =>
+export const useUploadFile = (t: Props) =>
   useForm<UploadFileValuesType>({
     defaultValues: {
       image: undefined,
     },
     mode: 'onChange',
-    resolver: zodResolver(uploadFileSchema()),
+    resolver: zodResolver(uploadFileSchema(t)),
   })
