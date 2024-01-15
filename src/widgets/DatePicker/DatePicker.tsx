d01259259e9cb56b2abe26a867f54d2fc7120bbc
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-15 15:11:58 +0300

    refactor: refactor DatePicker, add children and remove Date Input

--- src/widgets/DatePicker/DatePicker.tsx
+++ src/widgets/DatePicker/DatePicker.tsx
@@ -1,59 +1,45 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import { DateRange } from 'react-day-picker'
 
 import { inter } from '@/application'
-import { Calendar, DateInput } from '@/shared'
+import { Calendar } from '@/shared'
 import * as RadixDropDown from '@radix-ui/react-dropdown-menu'
-import { format } from 'date-fns'
 
 import s from './DatePicker.module.scss'
 
-type Props = {
-  fullWidth?: boolean
+export type DatePickerProps = {
+  children: ReactNode
+  daysRange?: DateRange | undefined
   mode: 'range' | 'single'
+  selectedDay?: Date | undefined
+  setDaysRange?: (daysRange: DateRange | undefined) => void
+  setSelectedDay?: (day: Date | undefined) => void
 }
 
-const getDateInputValue = (
-  mode: 'range' | 'single',
-  selected?: Date | undefined,
-  range?: DateRange | undefined
-) => {
-  if (mode === 'single' && selected) {
-    return format(selected, 'dd/MM/yyyy')
-  }
-  if (mode === 'range' && range) {
-    const from = range.from && format(range.from, 'dd/MM/yyyy')
-    const to = range.to && format(range.to, 'dd/MM/yyyy')
-
-    return `${from || '00/00/0000'} - ${to || '00/00/0000'}`
-  }
-
-  return ''
-}
-
-export const DatePicker = ({ fullWidth, mode }: Props) => {
+export const DatePicker = ({
+  children,
+  daysRange,
+  mode,
+  selectedDay,
+  setDaysRange,
+  setSelectedDay,
+}: DatePickerProps) => {
   const [open, setOpen] = useState(false)
-  const [selected, setSelected] = useState<Date>()
-  const [range, setRange] = useState<DateRange | undefined>()
-
-  const dateValue = getDateInputValue(mode, selected, range)
 
   return (
     <RadixDropDown.Root onOpenChange={setOpen} open={open}>
       <RadixDropDown.Trigger asChild>
-        <div className={s.trigger}>
-          <DateInput fullWidth mode={mode} placeholder={'00/00/0000'} value={dateValue} />
-        </div>
+        <div className={s.trigger}>{children}</div>
       </RadixDropDown.Trigger>
       <RadixDropDown.Portal>
         <RadixDropDown.Content align={'start'} className={s.content}>
           <Calendar
             className={inter.className}
             mode={mode}
-            range={range}
-            selected={selected}
-            setRange={setRange}
-            setSelected={setSelected}
+            range={daysRange}
+            selected={selectedDay}
+            setRange={setDaysRange}
+            setSelected={setSelectedDay}
           />
         </RadixDropDown.Content>
       </RadixDropDown.Portal>
