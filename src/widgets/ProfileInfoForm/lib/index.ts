6b38de308d8d9caba5cdd755e5fe7bfd6d127f89
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-15 11:57:02 +0300

    feat: add useProfileForm

---
+++ src/widgets/ProfileInfoForm/lib/index.ts
@@ -0,0 +1,1 @@
+export { useProfileForm } from './useProfileForm'
---
+++ src/widgets/ProfileInfoForm/lib/useProfileForm.ts
@@ -0,0 +1,49 @@
+import { useForm } from 'react-hook-form'
+
+import { ABOUT_ME_PATTERN, USER_FIRST_LAST_NAME_PATTERN, USERNAME_PATTERN } from '@/shared'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+type UserProfileData = {
+  aboutMe: string
+  city: string
+  country: string
+  dateOfBirth: string
+  firstName: string
+  lastName: string
+  username: string
+}
+
+const userProfileSchema = () =>
+  z.object({
+    aboutMe: z.string().trim().max(200, 'Error max length').regex(ABOUT_ME_PATTERN, 'Regex error'),
+    city: z.string(),
+    country: z.string(),
+    firstName: z
+      .string()
+      .trim()
+      .min(1, 'Error min length')
+      .max(50, 'Error max length')
+      .regex(USER_FIRST_LAST_NAME_PATTERN, 'Regex error'),
+    lastName: z
+      .string()
+      .trim()
+      .min(1, 'Error min length')
+      .max(50, 'Error max length')
+      .regex(USER_FIRST_LAST_NAME_PATTERN, 'Regex error'),
+    username: z
+      .string()
+      .trim()
+      .min(6, 'Error min length')
+      .max(30, 'Error max length')
+      .regex(USERNAME_PATTERN, 'regex error'),
+  })
+
+export type UserProfileFormValuesType = z.infer<ReturnType<typeof userProfileSchema>>
+
+export const useProfileForm = (values: UserProfileData) =>
+  useForm<UserProfileFormValuesType>({
+    mode: 'onTouched',
+    resolver: zodResolver(userProfileSchema()),
+    values: values,
+  })
