8f0935080bdbb38662906aa443652256ae176e16
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-16 20:22:11 +0300

    feat: add countriesApi

--- src/shared/api/countriesApi.ts
+++ src/shared/api/countriesApi.ts
@@ -1,12 +1,17 @@
 import { SelectOptions } from '@/shared'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const GET_COUNTRIES_URL = 'https://countriesnow.space/api/v0.1/countries/info?returns=countries'
+const GET_COUNTRIES_URL = 'https://countriesnow.space/api/v0.1/countries'
 
-type GetCountriesResponseType = {
-  data: { name: string }[]
+type BaseResponseType<T> = {
+  data: T
   error: boolean
   msg: string
+}
+type GetCountriesResponseType = BaseResponseType<{ name: string }[]>
+type GetCitiesResponseType = BaseResponseType<string[]>
+type GetCitiesRequestType = {
+  country: string
 }
 
 export const countriesApi = createApi({
@@ -14,10 +19,27 @@ export const countriesApi = createApi({
     baseUrl: GET_COUNTRIES_URL,
   }),
   endpoints: build => ({
+    getCities: build.mutation<any, GetCitiesRequestType>({
+      query: body => ({
+        body,
+        method: 'POST',
+        url: '/cities',
+      }),
+      transformResponse: (result: GetCitiesResponseType) => {
+        if (result.error) {
+          return []
+        }
+
+        return result.data.map(city => ({
+          title: city,
+          value: city,
+        }))
+      },
+    }),
     getCountries: build.query<SelectOptions[], void>({
       query: () => ({
         method: 'GET',
-        url: '',
+        url: '/info?returns=countries',
       }),
       transformResponse: (result: GetCountriesResponseType) => {
         if (result.error) {
@@ -26,7 +48,7 @@ export const countriesApi = createApi({
 
         return result.data.map(country => ({
           title: country.name,
-          value: country.name.toLowerCase(),
+          value: country.name,
         }))
       },
     }),
@@ -34,4 +56,4 @@ export const countriesApi = createApi({
   reducerPath: 'countriesApi',
 })
 
-export const { useGetCountriesQuery } = countriesApi
+export const { useGetCitiesMutation, useGetCountriesQuery } = countriesApi
