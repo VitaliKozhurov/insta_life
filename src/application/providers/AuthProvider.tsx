d8ce632e9bc77d3e325f05d55045adcbb090a8b0
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-22 23:11:53 +0300

    refactor: refactor Auth Provider, remove unused states

--- src/application/providers/AuthProvider.tsx
+++ src/application/providers/AuthProvider.tsx
@@ -1,31 +1,12 @@
 import { ReactNode, createContext } from 'react'
 
-import { Loader, PRIVATE_ROUTES, PUBLIC_ROUTES, Routes, useMeQuery } from '@/shared'
-import { useRouter } from 'next/router'
-export const AuthContext = createContext({ isAuth: false })
+import { useMeQuery } from '@/shared'
+
+export const AuthContext = createContext({ isAuth: false, isLoading: false })
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const router = useRouter()
-  const path = router.pathname as Routes
   const { isError, isLoading } = useMeQuery()
-
-  if (isLoading) {
-    return <Loader />
-  }
-
   const isAuth = !isError
 
-  if (!isAuth && PRIVATE_ROUTES.some(route => route.split('/')[1] === path.split('/')[1])) {
-    router.push(Routes.SIGN_IN)
-
-    return <Loader />
-  }
-
-  if (isAuth && PUBLIC_ROUTES.includes(path)) {
-    router.push(Routes.HOME)
-
-    return <Loader />
-  }
-
-  return <AuthContext.Provider value={{ isAuth }}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={{ isAuth, isLoading }}>{children}</AuthContext.Provider>
 }
